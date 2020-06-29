eb2ca2b3b94cb2529ee209f68d64d2044b35aee1
daviduntalan <daviduntalan@yahoo.com>
2020-06-29 12:03:49 +0800

    Added notification model

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -8,5 +8,17 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        printMessage("Hello, how are you?");
+        printMessage("I'm fine.");
+    }
+
+    private static void printMessage(String greetings) {
+        // alt + shift + v - extract into variable
+        // ctrl + alt + n - inline variable
+        System.out.println(greetings);
+        // expand selection - alt + shift + .
+        // shrink selection - alt + shift + ,
+        // alt + shift + m - extract to method
+        // ctrl + alt + p - extract to parameter
     }
 }
